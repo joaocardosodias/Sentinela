31b74698156f24acb1feaa257becbe00650749bc
joaocardosodias <joao.dias@sou.inteli.edu.br>
2026-07-20 17:27:17 -0300

    fix: redireciona para /solucao/

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,5 +3,5 @@ import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 
 export default function Home(): JSX.Element {
   const {withBaseUrl} = useBaseUrlUtils();
-  return <Redirect to={withBaseUrl('/solucao')} />;
+  return <Redirect to={withBaseUrl('/solucao/')} />;
 }
