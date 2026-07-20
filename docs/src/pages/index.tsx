04077bc2d0bd68c4a7592fa34fcaab7f4244a9c1
joaocardosodias <joao.dias@sou.inteli.edu.br>
2026-07-20 17:29:15 -0300

    fix: redirect hardcoded para /Sentinela/solucao/

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import {Redirect} from '@docusaurus/router';
-import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 
 export default function Home(): JSX.Element {
-  const {withBaseUrl} = useBaseUrlUtils();
-  return <Redirect to={withBaseUrl('/solucao/')} />;
+  return <Redirect to="/Sentinela/solucao/" />;
 }
